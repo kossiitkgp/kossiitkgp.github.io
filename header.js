ae56c91a6455e6592e1a4bf299f4077bcbb45bb2
americast <sayan.sinha@iitkgp.ac.in>
2017-05-05 21:32:24 +0530

    Rel ref fix

---
+++ header.js
@@ -0,0 +1,49 @@
+document.write('\
+    <!-- Following Menu -->\
+    \
+   <div class="ui large top menu fixed">\
+    <div class="ui container">\
+        <!--The menu items are not yet decided but it is recommended to add the page you are working on in this menu-->\
+        <!--and add the \'active\' class along with \'item\' class on your page name-->\
+        <div class="item"><img src="assets/images/kosslogo.png"></div>\
+        <a class="item" href="./index.html">Home</a>\
+        <a class="item" href="./other.html">Other</a>\
+        <div class="ui simple dropdown item">\
+            Dropdown <i class="dropdown icon"></i>\
+            <div class="menu">\
+                <a class="item" href="#">Link Item</a>\
+                <a class="item" href="#">Link Item</a>\
+                <div class="divider"></div>\
+                <div class="header">Header Item</div>\
+                <div class="item">\
+                    <i class="dropdown icon"></i>\
+                    Sub Menu\
+                    <div class="menu">\
+                        <a class="item" href="#">Link Item</a>\
+                        <a class="item" href="#">Link Item</a>\
+                    </div>\
+                </div>\
+                <a class="item" href="#">Link Item</a>\
+            </div>\
+        </div>\
+        <a class="item">Company</a>\
+        <a class="item">Careers</a>\
+        <div class="right menu">\
+            <div class="item">\
+                <a class="ui button" href="events/index.html">Events</a>\
+            </div>\
+            <div class="item">\
+                <a class="ui primary button">Talks</a>\
+            </div>\
+            <div class="item">\
+                <a class="ui button" href="projects/index.html">Projects</a>\
+            </div>\
+            <div class="item">\
+                <a class="ui button" href="help/index.html">Help</a>\
+            </div>\
+        </div>\
+    </div>\
+</div>\
+\
+\
+');
