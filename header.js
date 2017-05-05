876c256e0a74aebdab59d7c8be50d7846c475784
americast <sayan.sinha@iitkgp.ac.in>
2017-05-05 21:50:01 +0530

    Updated header

--- header.js
+++ header.js
@@ -5,42 +5,13 @@ document.write('\
     <div class="ui container">\
         <!--The menu items are not yet decided but it is recommended to add the page you are working on in this menu-->\
         <!--and add the \'active\' class along with \'item\' class on your page name-->\
-        <div class="item"><img src="assets/images/kosslogo.png"></div>\
-        <a class="item" href="./index.html">Home</a>\
-        <a class="item" href="./other.html">Other</a>\
-        <div class="ui simple dropdown item">\
-            Dropdown <i class="dropdown icon"></i>\
-            <div class="menu">\
-                <a class="item" href="#">Link Item</a>\
-                <a class="item" href="#">Link Item</a>\
-                <div class="divider"></div>\
-                <div class="header">Header Item</div>\
-                <div class="item">\
-                    <i class="dropdown icon"></i>\
-                    Sub Menu\
-                    <div class="menu">\
-                        <a class="item" href="#">Link Item</a>\
-                        <a class="item" href="#">Link Item</a>\
-                    </div>\
-                </div>\
-                <a class="item" href="#">Link Item</a>\
-            </div>\
-        </div>\
-        <a class="item">Company</a>\
-        <a class="item">Careers</a>\
+        <div class="item"><img src="../assets/images/kosslogo.png"></div>\
         <div class="right menu">\
-            <div class="item">\
-                <a class="ui button" href="events/index.html">Events</a>\
-            </div>\
-            <div class="item">\
-                <a class="ui primary button">Talks</a>\
-            </div>\
-            <div class="item">\
-                <a class="ui button" href="projects/index.html">Projects</a>\
-            </div>\
-            <div class="item">\
-                <a class="ui button" href="help/index.html">Help</a>\
-            </div>\
+        	<a class="item" href="../index.html">Home</a>\
+                <a class="item" href="../events/index.html">Events</a>\
+        	<a class="item" href="../projects/index.html">Projects</a>\
+                <a class="item" href="../team/index.html">Our Team</a>\
+                <a class="item"></a>\
         </div>\
     </div>\
 </div>\
