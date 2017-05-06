1bbace6ed8b1ca211affc512e6b018dc9241c3e3
Sayan Sinha <sayan.sinha@iitkgp.ac.in>
2017-05-06 00:03:31 -0600

    sep header footer (#37)
    * Added separate header and footer
    
    * Added sep header and footer
    
    * rel ref
    
    * Rel ref fix
    
    * Update header
    
    * Updated header
    
    * Update Projects
    
    * Updated Our Team
    
    * Update all pages
    
    * Only one place
    
    * Update README

---
+++ header.js
@@ -0,0 +1,20 @@
+function header(location)
+{
+    var home_loc="index.html";
+    var events_loc="events/index.html";
+    var projects_loc="projects/index.html";
+    var team_loc="team/index.html";
+    if (location==="home"){
+        string=
+			'<a class="active item" href="./'.concat(home_loc,'">Home</a><a class="item" href="./',events_loc,'">Events</a><a class="item" href="./',projects_loc,'">Projects</a><a class="item" href="./',team_loc,'">Our Team</a><a class="item"></a>');}
+    else if (location==="events"){
+            string=
+			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="active item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="item" href="../',team_loc,'">Our Team</a><a class="item"></a>');}
+    else if (location==="projects"){
+            string=
+			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',events_loc,'">Events</a><a class="active item" href="../',projects_loc,'">Projects</a><a class="item" href="../',team_loc,'">Our Team</a><a class="item"></a>');}
+    else if (location==="team"){
+            string=
+			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="active item" href="../',team_loc,'">Our Team</a><a class="item"></a>');}
+    document.write(string);
+}
