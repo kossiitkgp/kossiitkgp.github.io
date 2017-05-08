6577558fabee6cbc6780497a3ec41624a2f532f5
ankita132 <2ankitasahoo13@gmail.com>
2017-05-08 13:07:16 +0000

    nav bar changed,footer links added and the trail image changed

--- footer.js
+++ footer.js
@@ -3,21 +3,19 @@ document.write('\
         <div class="ui center aligned container">\
             <div class="ui stackable inverted divided grid">\
                 <div class="three wide column">\
-                    <h4 class="ui inverted header">Group 1</h4>\
+                    <h4 class="ui inverted header">Main Menu</h4>\
                     <div class="ui inverted link list">\
-                        <a href="#" class="item">Link One</a>\
-                        <a href="#" class="item">Link Two</a>\
-                        <a href="#" class="item">Link Three</a>\
-                        <a href="#" class="item">Link Four</a>\
+                        <a href="./index.html" class="item">Home</a>\
+                        <a href="./events/index.html" class="item">Events</a>\
+                        <a href="./team/index.html" class="item">Our Team</a>\
+                        <a href="./projects/index.html" class="item">Projects</a>\
                     </div>\
                 </div>\
                 <div class="three wide column">\
-                    <h4 class="ui inverted header">Group 2</h4>\
+                    <h4 class="ui inverted header">Contact Us</h4>\
                     <div class="ui inverted link list">\
-                        <a href="#" class="item">Link One</a>\
-                        <a href="#" class="item">Link Two</a>\
-                        <a href="#" class="item">Link Three</a>\
-                        <a href="#" class="item">Link Four</a>\
+                        <a href="https://www.facebook.com/kossiitkgp/" class="item">Facebook</a>\
+                        <a href="mailto:kossiitkgp@gmail.com" class="item">kossiitkgp@gmail.com</a>\
                     </div>\
                 </div>\
                 <div class="three wide column">\
@@ -35,7 +33,7 @@ document.write('\
                 </div>\
             </div>\
             <div class="ui inverted section divider"></div>\
-            <img src="../assets/images/kosslogo.png" class="ui centered mini image">\
+            <img src="./assets/images/kosslogo.png" class="ui centered mini image">\
             <div class="ui horizontal inverted small divided link list">\
                 <a class="item" href="#">Site Map</a>\
                 <a class="item" href="#">Contact Us</a>\
