c0c81c3fed06eb4e19d1474daca022bfa6c8f167
americast <sayan.sinha@iitkgp.ac.in>
2017-05-11 12:14:17 +0530

    Add About Us page

--- header.js
+++ header.js
@@ -8,21 +8,21 @@ function header(location)
     var contact_loc="contact/index.html";
     if (location==="home"){
         string=
-			'<a class="active item" href="./'.concat(home_loc,'">Home</a><a class="item" href="./',events_loc,'">Events</a><a class="item" href="./',projects_loc,'">Projects</a><a class="item" href="./',team_loc,'">Our Team</a><a class="item" href="./',help_loc,'">Help</a><a class="item" href="./',contact_loc,'">Contact Us</a>');}
+			'<a class="active item" href="./'.concat(home_loc,'">Home</a><a class="item" href="./',team_loc,'">About Us</a><a class="item" href="./',events_loc,'">Events</a><a class="item" href="./',projects_loc,'">Projects</a><a class="item" href="./',help_loc,'">Help</a><a class="item" href="./',contact_loc,'">Contact Us</a>');}
     else if (location==="events"){
             string=
-			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="active item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="item" href="../',team_loc,'">Our Team</a><a class="item" href="../',help_loc,'">Help</a><a class="item" href="../',contact_loc,'">Contact Us</a>');}
+			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',team_loc,'">About Us</a><a class="active item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="item" href="../',help_loc,'">Help</a><a class="item" href="../',contact_loc,'">Contact Us</a>');}
     else if (location==="projects"){
             string=
-			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',events_loc,'">Events</a><a class="active item" href="../',projects_loc,'">Projects</a><a class="item" href="../',team_loc,'">Our Team</a><a class="item" href="../',help_loc,'">Help</a><a class="item" href="../',contact_loc,'">Contact Us</a>');}
+			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',team_loc,'">About Us</a><a class="item" href="../',events_loc,'">Events</a><a class="active item" href="../',projects_loc,'">Projects</a><a class="item" href="../',help_loc,'">Help</a><a class="item" href="../',contact_loc,'">Contact Us</a>');}
     else if (location==="team"){
             string=
-			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="active item" href="../',team_loc,'">Our Team</a><a class="item" href="../',help_loc,'">Help</a><a class="item" href="../',contact_loc,'">Contact Us</a>');}
+			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="active item" href="../',team_loc,'">About Us</a><a class="item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="item" href="../',help_loc,'">Help</a><a class="item" href="../',contact_loc,'">Contact Us</a>');}
     else if (location==="help"){
             string=
-            '<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="item" href="../',team_loc,'">Our Team</a><a class="active item" href="../',help_loc,'">Help</a><a class="item" href="../',contact_loc,'">Contact Us</a>');}
+			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',team_loc,'">About Us</a><a class="item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="active item" href="../',help_loc,'">Help</a><a class="item" href="../',contact_loc,'">Contact Us</a>');}
     else if (location==="contact"){
             string=
-            '<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="item" href="../',team_loc,'">Our Team</a><a class="item" href="../',help_loc,'">Help</a><a class="active item" href="../',contact_loc,'">Contact Us</a>');}
+			'<a class="item" href="../'.concat(home_loc,'">Home</a><a class="item" href="../',team_loc,'">About Us</a><a class="item" href="../',events_loc,'">Events</a><a class="item" href="../',projects_loc,'">Projects</a><a class="item" href="../',help_loc,'">Help</a><a class="active item" href="../',contact_loc,'">Contact Us</a>');}
     document.write(string);
 }
