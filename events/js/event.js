8f3ce5d1c67335cbeadc299c748d39526738827c
ankita132 <2ankitasahoo13@gmail.com>
2017-05-19 19:04:19 +0000

    all secondary events added,automatic slideshow added

--- events/js/event.js
+++ events/js/event.js
@@ -1,22 +1,32 @@
-var slideIndex = 1;
+$( document ).ready(function() {
 
-$(document).ready(function () {
- if($('.mySlides').is(':visible')) {
-   showDivs(slideIndex);
-  }
+    var slidePosition=0;
+    var numOfSlide = $("#slide-holder > div").size();
+
+    $("#slide-holder").css("width",(numOfSlide*100)+"%");
+    $(".slide").css("width",(100/numOfSlide)+"%");
+
+    for (var a = 0; a < numOfSlide; a++) {
+        $('#slide-nav').append(' <a href="javascript: void(0)" class="slide-nav-bt'+(a===0?' active':'')+'">  </a> ');
+    }
+
+    $('.slide-nav-bt').click(function() {
+        moveSlide($(this));
+        clearInterval(autoPlaySlideInter);
+    });
+
+    function moveSlide(thisa) {
+        var thisindex = $('#slide-nav a').index(thisa);
+        $('#slide-holder').css("margin-left", '-'+thisindex +'00%');
+        $('#slide-nav a').removeClass('active');
+        thisa.addClass('active');
+    }
+
+    function autoPlaySlide() {
+        slidePosition++;
+        if (slidePosition == numOfSlide) { slidePosition = 0; }
+        moveSlide($("#slide-nav").children(".slide-nav-bt:eq("+slidePosition+")"));
+    }
+
+    var autoPlaySlideInter = setInterval(autoPlaySlide,3000);
 });
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
