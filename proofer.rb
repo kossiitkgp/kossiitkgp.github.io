618385b100622d7c01aabe079fd51f12025b14c2
americast <sayan.sinha@iitkgp.ac.in>
2017-07-04 16:40:55 +0530

    Add ignore 999 to proofer

--- proofer.rb
+++ proofer.rb
@@ -1,3 +1,3 @@
 require 'html-proofer'
-options =  { :assume_extension => true, :empty_alt_ignore => true, :checks_to_ignore =>["ImageCheck"]}
+options =  { :assume_extension => true, :empty_alt_ignore => true, :checks_to_ignore =>["ImageCheck"], :http_status_ignore => [999]}
 HTMLProofer.check_directory("./",options).run
