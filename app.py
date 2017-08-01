7a113c48039ab94fc4467d24938678d988acdc07
Sayan Sinha <sayan.sinha@iitkgp.ac.in>
2017-08-02 00:07:50 +0530

    Add_counter (#146)
    Add counter to homepage

---
+++ app.py
@@ -0,0 +1,118 @@
+import json
+import os
+import requests
+f=open("members.txt","r")
+repos=[]
+openissues=0
+closedissues=0
+openprs=0
+closedprs=0
+itercount=0
+while (1):
+            name=f.readline()
+            if not name: break
+            name=name.strip()
+            url = "https://api.github.com/graphql"
+            headers = {"Authorization": "Basic "+str(os.environ['OUATH_KEY'])}
+            repo_cur=issue_cur=pr_cur="first: 100"
+            while (1):
+                query = json.dumps({"query": "query{user(login: \"" + name +
+                                        "\") { repositories("+str(repo_cur)+"){edges{node{url isFork}} pageInfo{endCursor hasNextPage}} issues("+str(issue_cur)+")\
+                                        {edges{node{state}} pageInfo {endCursor hasNextPage}} pullRequests("+str(pr_cur)+"){edges{node{state}} pageInfo \
+                                        {endCursor hasNextPage}} }}"})
+                print("query",query)
+                r = requests.post(url, headers=headers, data=query)
+                data_dict = json.loads(str(r.content))
+                try:
+                    next_repo=data_dict['data']['user']['repositories']['pageInfo']['endCursor']
+                except:
+                    next_repo="errored"
+                try:
+                    next_issue=data_dict['data']['user']['issues']['pageInfo']['endCursor']
+                except:
+                    next_issue="errored"
+                try:
+                    next_pr=data_dict['data']['user']['pullRequests']['pageInfo']['endCursor']
+                except:
+                    next_pr="errored"
+
+                print ("data dict",data_dict)
+
+
+                if next_repo!="errored":
+                    for repo in data_dict['data']['user']['repositories']['edges']:
+                        if repo['node']['isFork']==False:
+                            repos.append(repo['node']['url'])
+                    if next_repo!=None and data_dict['data']['user']['repositories']['pageInfo']['hasNextPage']!=False:
+                        repo_cur='first:100 after:"'+str(next_repo)+'" '
+                    else:
+                        repo_cur='first: 0'
+
+                if next_issue!="errored":
+                    for issue in data_dict['data']['user']['issues']['edges']:
+                        if issue['node']['state']=='CLOSED':
+                            closedissues+=1
+                        else:
+                            openissues+=1
+                        if next_issue!=None and data_dict['data']['user']['issues']['pageInfo']['hasNextPage']!=False:
+                            issue_cur='first: 100 after:"'+str(next_issue)+'" '
+                        else:
+                            issue_cur='first: 0'
+
+                if next_pr!="errored":
+                    for pr in data_dict['data']['user']['pullRequests']['edges']:
+                        if pr['node']['state']=='MERGED':
+                            closedprs+=1
+                        elif pr['node']['state']=='OPEN':
+                            openprs+=1
+                        if next_pr!=None and data_dict['data']['user']['pullRequests']['pageInfo']['hasNextPage']!=False:
+                            pr_cur='first: 100 after:"'+str(next_pr)+'" '
+                        else:
+                            pr_cur='first: 0'
+                
+                if (data_dict['data']['user']['repositories']['pageInfo']['hasNextPage']==False or next_repo==None) and (data_dict['data']['user']\
+                    ['issues']['pageInfo']['hasNextPage']==False or next_issue==None) and (data_dict['data']['user']['pullRequests']['pageInfo']\
+                    ['hasNextPage']==False or next_pr==None): break
+                itercount+=1
+
+                    
+
+
+repos=set(repos[:2])
+print repos
+print "open issues: "+str(openissues)
+print "closed issues: "+str(closedissues)
+print "open prs: "+str(closedprs)
+print "closed prs: "+str(closedprs)
+print "repeated: "+str(itercount)
+# import os
+lines_of_code=0
+for repo in repos:
+  os.system("git clone --depth 1 "+str(repo)+".git ")
+  os.system("cloc "+str(repo).split('/')[-1]+" > out")
+  f=open("out","r")
+  alltext=f.read()
+  # print alltext
+  # print alltext.split(' ')[-1].split('-')[0]
+  try:
+    lines_of_code+=int(alltext.split(' ')[-1].split('-')[0])
+    os.system("rm -rf "+str(repo.split('/')[-1]))
+  except:
+    pass
+  print ("lines of code",lines_of_code)
+
+print lines_of_code
+
+f=open("index.tmpl","r")
+all_lines=f.read()
+all_lines=all_lines.replace("<!-- Open PRs -->",str(openprs))
+all_lines=all_lines.replace("<!-- Closed PRs -->",str(closedprs))
+all_lines=all_lines.replace("<!-- Open Issues -->",str(openissues))
+all_lines=all_lines.replace("<!-- Closed Issues -->",str(closedissues))
+all_lines=all_lines.replace("<!-- Lines -->",str(lines_of_code))
+f=open("index.html","r")
+all_prev_lines=f.read()
+if all_prev_lines!=all_lines:
+    f=open("index.html","w")
+    os.system("git pull --rebase origin master")
+    f.write(all_lines)
---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "contact@kossiitkgp.in"
+  git config --global user.name "KOSS"
+}
+
+commit_website_files() {
+  git checkout count
+  git add index.html
+  git commit --message "update no"
+}
+
+upload_files() {
+  git remote add origin-here https://${OUATH_KEY}@github.com/kossiitkgp/kossiitkgp.github.io.git
+  git push --quiet --set-upstream origin-here master 
+}
+
+setup_git
+commit_website_files
+upload_files
