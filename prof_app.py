f127ab8c71ab42c8fb1d25dff9d83b65bcd80a74
Dibya Das <dibyadascool@gmail.com>
2017-10-01 19:26:30 +0530

    removed pep8 errors

--- prof_app.py
+++ prof_app.py
@@ -5,12 +5,11 @@ from collections import OrderedDict
 
 import jinja2
 
-def render_jinja_html(template_loc,file_name,**context):
 
+def render_jinja_html(template_loc, file_name, **context):
     return jinja2.Environment(
         loader=jinja2.FileSystemLoader(template_loc+'/')
     ).get_template(file_name).render(context)
-
 
 
 def profile(name, position=None):
@@ -31,18 +30,19 @@ def profile(name, position=None):
     email = data_dict['data']['user']['email']
     blog = data_dict['data']['user']['websiteUrl']
 
-    x = render_jinja_html('templates','template.tmpl', name=name,
-                        pinned_repos=pinned_repos,
-                        image=image,
-                        gh_link=gh_link,
-                        bio=bio,
-                        full_name=full_name,
-                        email=email,
-                        blog=blog,
-                        position=position
-                        )
+    x = render_jinja_html('templates', 'template.tmpl', name=name,
+                          pinned_repos=pinned_repos,
+                          image=image,
+                          gh_link=gh_link,
+                          bio=bio,
+                          full_name=full_name,
+                          email=email,
+                          blog=blog,
+                          position=position
+                          )
     with open("profiles/" + name + ".html", "w") as f:
         f.write(x)
+
 
 def generate(id=None):
     if id is not None:
